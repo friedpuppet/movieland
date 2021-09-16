758934f474afb7666a8c52b202a0e2c40e24c324
Ievgen Yelisieiev <friedpuppet@pm.me>
2021-09-16 19:46:09 +0300

    Feature [x-1] Parallel enrichment for movie [+] MovieService.getSingle() method fills countries, genres, reviews in parrallel

--- src/main/java/ua/yelisieiev/service/DefaultMovieService.java
+++ src/main/java/ua/yelisieiev/service/DefaultMovieService.java
@@ -1,22 +1,70 @@
 package ua.yelisieiev.service;
 
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ua.yelisieiev.dao.MovieDao;
 import ua.yelisieiev.entity.*;
 
 import java.util.List;
+import java.util.concurrent.*;
+import java.util.function.Supplier;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class DefaultMovieService implements MovieService {
+    private static final int DEFAULT_ENRICHMENT_TIMEOUT_MS = 5000;
+    @Value("movie.enrichment.parallel.timeout")
+    private int EnrichmentTimeout = DEFAULT_ENRICHMENT_TIMEOUT_MS;
     private final MovieDao movieDao;
     private final CountryService countryService;
     private final GenreService genreService;
     private final ReviewService reviewService;
+    private final ExecutorService executorService = Executors.newCachedThreadPool();
+
+    @RequiredArgsConstructor
+    private static class FieldFetcherThread<T> implements Callable<T> {
+        private final Supplier<T> supplier;
+        private final CountDownLatch latch;
+
+        @Override
+        public T call() {
+            T value = supplier.get();
+            latch.countDown();
+            //todo it's possible than the latch is open but the value isn't returned yet
+            // it's considered a timeout with current implementation
+            return value;
+        }
+    }
+
+    @RequiredArgsConstructor
+    @Slf4j
+    private class FieldFetcher<T> {
+        private final Supplier<T> supplier;
+        private final CountDownLatch latch;
+        private Future<T> future;
+
+        public void run() {
+            future = executorService.submit(
+                    new FieldFetcherThread<>(supplier, latch));
+        }
+
+        public T get() {
+            if (future.isDone()) {
+                try {
+                    return future.get();
+                } catch (InterruptedException e) {
+                    log.warn("Field fetch was interrupted", e);
+                } catch (ExecutionException e) {
+                    log.warn("Field fetch unsuccessful", e);
+                }
+            }
+            future.cancel(true);
+            return null;
+        }
+    }
 
     @Override
     public List<Movie> getAll() {
@@ -49,12 +97,30 @@ public class DefaultMovieService implements MovieService {
     @Override
     public MovieFull getSingle(int id) {
         MovieFull movieFull = movieDao.getSingle(id);
-        List<Country> countries = countryService.getListForMovie(id);
-        List<Genre> genres = genreService.getListForMovie(id);
-        List<Review> reviews = reviewService.getListForMovie(id);
-        movieFull.setCountries(countries);
-        movieFull.setGenres(genres);
-        movieFull.setReviews(reviews);
+
+        CountDownLatch latch = new CountDownLatch(3);
+
+        FieldFetcher<List<Country>> countriesFetcher = new FieldFetcher<>(() -> countryService.getListForMovie(id), latch);
+        countriesFetcher.run();
+
+        FieldFetcher<List<Genre>> genresFetcher = new FieldFetcher<>(() -> genreService.getListForMovie(id), latch);
+        genresFetcher.run();
+
+        FieldFetcher<List<Review>> reviewsFetcher = new FieldFetcher<>(() -> reviewService.getListForMovie(id), latch);
+        reviewsFetcher.run();
+
+        try {
+            if (!latch.await(EnrichmentTimeout, TimeUnit.MILLISECONDS)) {
+                log.warn("Parallel enrichment timed out. Will try to enrich entity with what is available");
+            }
+        } catch (InterruptedException e) {
+            log.warn("Parallel enrichment was interrupted. Will try to enrich entity with what is available", e);
+        }
+
+        movieFull.setCountries(countriesFetcher.get());
+        movieFull.setGenres(genresFetcher.get());
+        movieFull.setReviews(reviewsFetcher.get());
+
         return movieFull;
     }
 }
